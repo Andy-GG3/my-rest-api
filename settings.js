120e060ae1e78dd11a32c7afc8bce6867b9f3c9c
Andy-GG3 <118534297+Andy-GG3@users.noreply.github.com>
2024-05-20 14:29:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = '👑AndyXD👑'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Gg:Andy1955@cluster0.dyqlccs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "andyxd1955@gmail.com" //email
+global.email_password = "hynfdsvqibgfcmxq" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
